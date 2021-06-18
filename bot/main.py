a57b51b610344600d037d62245ca2ceec63276f2
bfaulkn <bfaulkn@siue.edu>
2021-06-17 20:57:50 -0800

    Add some other joke types

--- bot/main.py
+++ bot/main.py
@@ -18,6 +18,8 @@ dad_jokes = [
     "Are you fucking kidding me? Watch your mouth."
 ]
 
+headers = {'user-agent': 'no-daddy/0.0.1', 'Accept': 'application/json'}
+
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
@@ -27,10 +29,27 @@ def get_quote():
 
 
 def get_dad_joke():
-    headers = {'user-agent': 'no-daddy/0.0.1', 'Accept': 'application/json'}
     response = requests.get("https://icanhazdadjoke.com/", headers=headers)
     json_data = json.loads(response.text)
     return json_data['joke']
+
+
+def get_yo_momma_joke():
+    response = requests.get("https://api.yomomma.info/", headers=headers)
+    json_data = json.loads(response.text)
+    return json_data['joke']
+
+
+def get_chuck_norris_joke():
+    response = requests.get("https://api.chucknorris.io/jokes/random", headers=headers)
+    json_data = json.loads(response.text)
+    return json_data['value']
+
+
+def get_2_part_joke():
+    response = requests.get("https://official-joke-api.appspot.com/jokes/programming/random", headers=headers)
+    json_data = json.loads(response.text)[0]
+    return json_data['setup'], json_data['punchline']
 
 
 @client.event
@@ -45,16 +64,23 @@ async def on_message(message):
 
     msg = message.content.lower()
 
-    if msg.startswith('$inspire'):
-        quote = get_quote()
-        await message.channel.send(quote)
+    # if msg.startswith('$inspire'):
+    #     quote = get_quote()
+    #     await message.channel.send(quote)
 
     if msg.startswith('$dad'):
-        dj = get_dad_joke()
-        await message.channel.send(dj)
+        await message.channel.send(get_dad_joke())
+    if msg.startswith('$mom'):
+        await message.channel.send(get_yo_momma_joke())
+    if msg.startswith('$chuck'):
+        await message.channel.send(get_chuck_norris_joke())
+    if msg.startswith('$joke'):
+        setup, punchline = get_2_part_joke()
+        await message.channel.send(setup)
+        await message.channel.send(f"/spoiler {punchline}")
 
-    if any(word in msg for word in potty_words):
-        await message.channel.send(random.choice(dad_jokes))
+    # if any(word in msg for word in potty_words):
+    #     await message.channel.send(random.choice(dad_jokes))
 
 token = os.getenv('TOKEN')
 client.run(token)
